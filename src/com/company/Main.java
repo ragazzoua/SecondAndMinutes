06ea3708f45cbd74da1d0c01941f528c8162fca0
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-15 15:41:38 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(60,0));
+        System.out.println(getDurationString(60, 0));
     }
 
     private static String getDurationString(long minutes, long seconds) {
@@ -13,6 +13,5 @@ public class Main {
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
         return hours + "h " + remainingMinutes + "m " + seconds + "s";
-
     }
 }
